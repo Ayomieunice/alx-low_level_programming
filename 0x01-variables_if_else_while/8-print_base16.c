6df732330a310372bd3a6c899ff4d7a746b79563
Ayomieuice <euniceakintemi@gmail.com>
2023-04-23 10:48:23 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 0 to 9
+ * followed by lowercase letter a to f
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
