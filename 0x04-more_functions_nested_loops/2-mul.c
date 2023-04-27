85a39da3c6c425cefd00f9781d76252b6f84ec68
Ayomieuice <euniceakintemi@gmail.com>
2023-04-27 14:02:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mull- Multiple two integers
+ * @x: The first number to be multiply
+ * @y: The second number to multiply with first number
+ * Return: The result of multiplication between x paran and y paran
+ */
+
+int mull(int x, int y)
+
+{
+	return (x= y);
+}
